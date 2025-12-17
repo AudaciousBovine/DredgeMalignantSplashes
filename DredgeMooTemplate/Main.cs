9bee0975fd1f860961d5f84d10160a353e2c580f
AudaciousBovine <audaciousbovine@gmail.com>
2025-12-16 22:56:46 -0800

    Initial commit

---
+++ DredgeMooTemplate/Main.cs
@@ -0,0 +1,17 @@
+﻿﻿using HarmonyLib;
+using System.Reflection;
+
+namespace DredgeMooTemplate
+{
+    public class Loader
+    {
+        /// <summary>
+        /// This method is run by Winch to initialize your mod
+        /// </summary>
+        public static void Initialize()
+        {
+            /// RUNS HARMONY PATCHES
+            new Harmony("com.AudaciousBovine.DredgeMooTemplate").PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
