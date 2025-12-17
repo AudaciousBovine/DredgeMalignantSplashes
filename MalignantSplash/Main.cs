508f84786c70c9ec4f1e915d78bace013aa59be2
AudaciousBovine <audaciousbovine@gmail.com>
2025-12-17 02:28:48 -0800

    First Release Ready
    Got it working

--- MalignantSplash/Main.cs
+++ MalignantSplash/Main.cs
@@ -1,17 +1,30 @@
-﻿﻿using HarmonyLib;
+﻿﻿﻿using System.Linq;
+using Winch.Util;
+using HarmonyLib;
 using System.Reflection;
+using UnityEngine;
+using Winch.Config;
+using UnityEngine.Localization.SmartFormat.Utilities;
+using Winch.Core;
 
 namespace MalignantSplash
 {
     public class Loader
     {
-        /// <summary>
         /// This method is run by Winch to initialize your mod
-        /// </summary>
         public static void Initialize()
         {
             /// RUNS HARMONY PATCHES
             new Harmony("com.AudaciousBovine.MalignantSplash").PatchAll(Assembly.GetExecutingAssembly());
+            /// Runs Game Loaded Method
+            ApplicationEvents.Instance.OnGameLoaded += OnGameLoaded;
+        }
+        
+            // This method enables the shipwright to buy materials
+        private static void OnGameLoaded()
+        {
+            var shipwrightDestination = DockUtil.GetAllShipyardDestinations().FirstOrDefault(shipyard => shipyard.Id == "destination.gm-shipwright");
+            shipwrightDestination.itemSubtypesBought |= ItemSubtype.MATERIAL;
         }
     }
 }
---
+++ MalignantSplash/Splash/Splash_Patch.cs
@@ -0,0 +1,35 @@
+﻿﻿using System.Linq;
+using Winch.Util;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+using Winch.Config;
+using UnityEngine.Localization.SmartFormat.Utilities;
+using Winch.Core;
+
+namespace MalignantSplash.Splash.Patches
+{
+    [HarmonyPatch(typeof(GridUI))]
+    public static class Splash_Patch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("CreateObject")]
+        public static void CreateObjectPrefix(GridUI __instance, SpatialItemInstance entry)
+        {
+            /// Getting Dark Splash Item
+            if (entry.id == "dark-splash")
+            {
+                /// Setting the item data to a variable for easier use
+                SpatialItemData dark_splash = entry.GetItemData<SpatialItemData>();
+                if (dark_splash != null)
+                {
+                    /// Changing item properties
+                    dark_splash.canBeDiscardedByPlayer = false;                    
+                    dark_splash.canBeSoldByPlayer = true;
+                    dark_splash.value = -1;
+                    dark_splash.itemSubtype = ItemSubtype.MATERIAL;
+                }
+            }
+        }
+    }
+}
